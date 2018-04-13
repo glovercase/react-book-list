2e2e75c0dc6c07301500afed89c3c70cd381963e
Casey Glover <casey.n.glover@gmail.com>
2018-04-13 12:50:27 +1200

    Book list using react

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Lord of the Rings", author: "J. R. R. Tolkien" },
+    { title: "Harry Potter", author: "J. K. Rowling" },
+    { title: "The Dark Tower", author: "J. R. R. Tolkien" },
+    { title: "Its not about the bike", author: "Lance Armstrong" }
+  ];
+}
